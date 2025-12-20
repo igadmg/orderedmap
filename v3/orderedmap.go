b2b64c472d370aa69ee83f6a99613e0152f44b15
Igor Karatayev <igor.cwer@gmail.com>
2025-12-20 13:43:06 +0300

    make orderedmap value type, just like normal map.

--- v3/orderedmap.go
+++ v3/orderedmap.go
@@ -4,12 +4,20 @@ import "iter"
 
 type OrderedMap[K comparable, V any] struct {
 	kv map[K]*Element[K, V]
-	ll list[K, V]
+	ll *list[K, V]
+}
+
+func MakeOrderedMap[K comparable, V any]() OrderedMap[K, V] {
+	return OrderedMap[K, V]{
+		kv: make(map[K]*Element[K, V]),
+		ll: new(list[K, V]),
+	}
 }
 
 func NewOrderedMap[K comparable, V any]() *OrderedMap[K, V] {
 	return &OrderedMap[K, V]{
 		kv: make(map[K]*Element[K, V]),
+		ll: new(list[K, V]),
 	}
 }
 
@@ -18,6 +26,7 @@ func NewOrderedMap[K comparable, V any]() *OrderedMap[K, V] {
 func NewOrderedMapWithCapacity[K comparable, V any](capacity int) *OrderedMap[K, V] {
 	return &OrderedMap[K, V]{
 		kv: make(map[K]*Element[K, V], capacity),
+		ll: new(list[K, V]),
 	}
 }
 
@@ -31,7 +40,7 @@ func NewOrderedMapWithElements[K comparable, V any](els ...*Element[K, V]) *Orde
 
 // Get returns the value for a key. If the key does not exist, the second return
 // parameter will be false and the value will be nil.
-func (m *OrderedMap[K, V]) Get(key K) (value V, ok bool) {
+func (m OrderedMap[K, V]) Get(key K) (value V, ok bool) {
 	v, ok := m.kv[key]
 	if ok {
 		value = v.Value
@@ -43,7 +52,7 @@ func (m *OrderedMap[K, V]) Get(key K) (value V, ok bool) {
 // Set will set (or replace) a value for a key. If the key was new, then true
 // will be returned. The returned value will be false if the value was replaced
 // (even if the value was the same).
-func (m *OrderedMap[K, V]) Set(key K, value V) bool {
+func (m OrderedMap[K, V]) Set(key K, value V) bool {
 	_, alreadyExist := m.kv[key]
 	if alreadyExist {
 		m.kv[key].Value = value
@@ -58,7 +67,7 @@ func (m *OrderedMap[K, V]) Set(key K, value V) bool {
 // ReplaceKey replaces an existing key with a new key while preserving order of
 // the value. This function will return true if the operation was successful, or
 // false if 'originalKey' is not found OR 'newKey' already exists (which would be an overwrite).
-func (m *OrderedMap[K, V]) ReplaceKey(originalKey, newKey K) bool {
+func (m OrderedMap[K, V]) ReplaceKey(originalKey, newKey K) bool {
 	element, originalExists := m.kv[originalKey]
 	_, newKeyExists := m.kv[newKey]
 	if originalExists && !newKeyExists {
@@ -72,7 +81,7 @@ func (m *OrderedMap[K, V]) ReplaceKey(originalKey, newKey K) bool {
 
 // GetOrDefault returns the value for a key. If the key does not exist, returns
 // the default value instead.
-func (m *OrderedMap[K, V]) GetOrDefault(key K, defaultValue V) V {
+func (m OrderedMap[K, V]) GetOrDefault(key K, defaultValue V) V {
 	if value, ok := m.kv[key]; ok {
 		return value.Value
 	}
@@ -82,7 +91,7 @@ func (m *OrderedMap[K, V]) GetOrDefault(key K, defaultValue V) V {
 
 // GetElement returns the element for a key. If the key does not exist, the
 // pointer will be nil.
-func (m *OrderedMap[K, V]) GetElement(key K) *Element[K, V] {
+func (m OrderedMap[K, V]) GetElement(key K) *Element[K, V] {
 	element, ok := m.kv[key]
 	if ok {
 		return element
@@ -92,13 +101,13 @@ func (m *OrderedMap[K, V]) GetElement(key K) *Element[K, V] {
 }
 
 // Len returns the number of elements in the map.
-func (m *OrderedMap[K, V]) Len() int {
+func (m OrderedMap[K, V]) Len() int {
 	return len(m.kv)
 }
 
 // AllFromFront returns an iterator that yields all elements in the map starting
 // at the front (oldest Set element).
-func (m *OrderedMap[K, V]) AllFromFront() iter.Seq2[K, V] {
+func (m OrderedMap[K, V]) AllFromFront() iter.Seq2[K, V] {
 	return func(yield func(key K, value V) bool) {
 		for el := m.Front(); el != nil; el = el.Next() {
 			if !yield(el.Key, el.Value) {
@@ -110,7 +119,7 @@ func (m *OrderedMap[K, V]) AllFromFront() iter.Seq2[K, V] {
 
 // AllFromBack returns an iterator that yields all elements in the map starting
 // at the back (most recent Set element).
-func (m *OrderedMap[K, V]) AllFromBack() iter.Seq2[K, V] {
+func (m OrderedMap[K, V]) AllFromBack() iter.Seq2[K, V] {
 	return func(yield func(key K, value V) bool) {
 		for el := m.Back(); el != nil; el = el.Prev() {
 			if !yield(el.Key, el.Value) {
@@ -123,7 +132,7 @@ func (m *OrderedMap[K, V]) AllFromBack() iter.Seq2[K, V] {
 // Keys returns an iterator that yields all the keys in the map starting at the
 // front (oldest Set element). To create a slice containing all the map keys,
 // use the slices.Collect function on the returned iterator.
-func (m *OrderedMap[K, V]) Keys() iter.Seq[K] {
+func (m OrderedMap[K, V]) Keys() iter.Seq[K] {
 	return func(yield func(key K) bool) {
 		for el := m.Front(); el != nil; el = el.Next() {
 			if !yield(el.Key) {
@@ -136,7 +145,7 @@ func (m *OrderedMap[K, V]) Keys() iter.Seq[K] {
 // Values returns an iterator that yields all the values in the map starting at
 // the front (oldest Set element). To create a slice containing all the map
 // values, use the slices.Collect function on the returned iterator.
-func (m *OrderedMap[K, V]) Values() iter.Seq[V] {
+func (m OrderedMap[K, V]) Values() iter.Seq[V] {
 	return func(yield func(value V) bool) {
 		for el := m.Front(); el != nil; el = el.Next() {
 			if !yield(el.Value) {
@@ -148,7 +157,7 @@ func (m *OrderedMap[K, V]) Values() iter.Seq[V] {
 
 // Delete will remove a key from the map. It will return true if the key was
 // removed (the key did exist).
-func (m *OrderedMap[K, V]) Delete(key K) (didDelete bool) {
+func (m OrderedMap[K, V]) Delete(key K) (didDelete bool) {
 	element, ok := m.kv[key]
 	if ok {
 		m.ll.Remove(element)
@@ -160,19 +169,19 @@ func (m *OrderedMap[K, V]) Delete(key K) (didDelete bool) {
 
 // Front will return the element that is the first (oldest Set element). If
 // there are no elements this will return nil.
-func (m *OrderedMap[K, V]) Front() *Element[K, V] {
+func (m OrderedMap[K, V]) Front() *Element[K, V] {
 	return m.ll.Front()
 }
 
 // Back will return the element that is the last (most recent Set element). If
 // there are no elements this will return nil.
-func (m *OrderedMap[K, V]) Back() *Element[K, V] {
+func (m OrderedMap[K, V]) Back() *Element[K, V] {
 	return m.ll.Back()
 }
 
 // Copy returns a new OrderedMap with the same elements.
 // Using Copy while there are concurrent writes may mangle the result.
-func (m *OrderedMap[K, V]) Copy() *OrderedMap[K, V] {
+func (m OrderedMap[K, V]) Copy() *OrderedMap[K, V] {
 	m2 := NewOrderedMapWithCapacity[K, V](m.Len())
 	for el := m.Front(); el != nil; el = el.Next() {
 		m2.Set(el.Key, el.Value)
@@ -181,7 +190,7 @@ func (m *OrderedMap[K, V]) Copy() *OrderedMap[K, V] {
 }
 
 // Has checks if a key exists in the map.
-func (m *OrderedMap[K, V]) Has(key K) bool {
+func (m OrderedMap[K, V]) Has(key K) bool {
 	_, exists := m.kv[key]
 	return exists
 }
